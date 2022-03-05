0bd12d8cb60cc2dcfecde130d6f7755ffc0392a0
elifnazceyhan <96650842+elifnazceyhan@users.noreply.github.com>
2022-03-05 13:55:09 +0300

    Add files via upload

---
+++ Araba fonksiyon.py
@@ -0,0 +1,12 @@
+#Arabanın markasını, modelini, rengini, plakasını yazdıran fonksiyona plaka göndermediğimizdede 3 değer üzerinden işlem yapan fonksiyonu yazınız.
+def bilgilerigoster(marka,model,renk,plaka):
+    print("Arabanın markası " ,marka)
+    print("Arabanın modeli ", model)
+    print("Arabanın rengi ", renk)
+    print("Arabanın plakası", plaka)
+marka=input("Arabanın markasını giriniz:")
+model=input("Arabanın modelini giriniz:")
+renk=input("Arabanın rengini giriniz:")
+plaka=input("Arabanın plakasını giriniz:")
+bilgilerigoster(marka=marka,model=model,renk=renk,plaka=plaka)
+
